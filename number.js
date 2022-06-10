58380252e0d976ea229608c191314da2a4b4565c
fuzhicong <1447503159@qq.com>
2022-06-11 01:33:03 +0800

    jstest

---
+++ number.js
@@ -0,0 +1,18 @@
+<script>
+          var i;
+          for(i = 1;i <= 20;i ++){
+            
+		        if (i%2==1) {
+			          document.write(i+' is odd number.'+'<br>')
+		        }else{
+              if(i!=20){
+                document.write(i+' is even number.'+'<br>')
+              }else{
+                document.write(i+' is even number.')
+              }
+			          
+	        	}
+          }
+
+
+    </script>
